291ea13f31fea9592f95c1d81039928076279469
roscaandreea <andreea.rosca@info.uaic.ro>
2021-09-30 15:19:39 +0300

    Added Button component

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,21 @@ import styled from 'styled-components';
 function Section() {
     return (
         <Wrap>
-            
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        Custom Order
+                    </LeftButton>
+                    <RightButton>
+                        Existing Inventory
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     )
 }
@@ -12,11 +26,49 @@ function Section() {
 export default Section;
 
 const Wrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //vertical
+    align-items: center; //horizontal
     width: 100vw;
     height: 100vh;
     background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
     background-image: url('/images/model-s.jpg');
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+const Buttons = styled.div`
 
+`
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
 
+const LeftButton = styled.div`
+    background-color: rgba(23,26,32,0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+const RightButton = styled(LeftButton)`
 
+`
+const DownArrow = styled.img`
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
 `
