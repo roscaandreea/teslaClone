a491ba9848ed361217efcaf2d82378e514471a4d
roscaandreea <andreea.rosca@info.uaic.ro>
2021-10-06 16:18:00 +0300

    Created BurgerNav + methods to enter and close this nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import DehazeIcon from '@mui/icons-material/Dehaze';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function Header() {
+    const [burgerStatus,setBurgerStatus] = useState(false);
     return (
         <Container>
             <a>
@@ -18,8 +20,20 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu />
+                <CustomMenu onClick = { () => setBurgerStatus(true)} />
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick = { () => setBurgerStatus(false)} />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadaster</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -35,6 +49,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;
 `
 const Menu = styled.div`
     display: flex;
@@ -62,4 +77,33 @@ const RightMenu = styled.div`
 `
 const CustomMenu = styled(DehazeIcon)`
     cursor: pointer;
-`  
+`
+const BurgerNav = styled.div`
+    position:fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0,0,0,0.2);
+        a {
+            font-weight: 600;
+        }
+    }
+`
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
